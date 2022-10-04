64da79525e9a2c17fc491c17120975fa3c3394cc
46703 <46703@DESKTOP-FIH4LKA>
2022-10-04 17:45:52 +0200

    Add project files.

---
+++ Uppgift 3.14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Inlämningsuppgift
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (Ja eller Nej)");
+            Console.Write("Svara här:  "); string svar1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Hur gammal är du?");
+            Console.Write("Svara här:  "); int svar2 = int.Parse(Console.ReadLine());
+
+            char[] svar = svar1.ToCharArray();
+
+            if (svar[1] == 'j' && svar2 < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+
+
+            }
+        }
+    }
+}
